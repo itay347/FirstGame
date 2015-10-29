dba1eda3eabfe821fb11f4289be3a2c28b48471b
Itay Mastor <itay647@gmail.com>
2015-10-29 19:00:16 +0200

    Initial commit

---
+++ desktop/src/com/itayandtamir/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.itayandtamir.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.itayandtamir.game.FirstGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FirstGame(), config);
+	}
+}
