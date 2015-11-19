96479c50076fd2ceba3e3837e9d24cad0d042de1
Itay Mastor <itay647@gmail.com>
2015-11-19 23:39:12 +0200

    Added boat lanes & bug fixes - Now the boat is able to move lanes by pressing one of the right middle or left lane. - change the boat's Y-axis velocity to 10. - fixed when debugging on an android device the game is on landscape mode (changed to portrait).

--- core/src/com/itayandtamir/game/Sprites/Boat.java
+++ core/src/com/itayandtamir/game/Sprites/Boat.java
@@ -2,6 +2,7 @@ package com.itayandtamir.game.Sprites;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Boat {
     private Vector2 position;
@@ -9,15 +10,15 @@ public class Boat {
     private Texture texture;
 
     public Boat(float x, float y){
-        position = new Vector2(x,y);
-        velocity = new Vector2(0, 5);
+        position = new Vector2(x, y);
+        velocity = new Vector2(0, 10);
         texture = new Texture("Boat.png");
     }
 
     public void update(float dt){
         velocity.scl(dt);
         position.add(0, velocity.y);
-        velocity.scl(1/dt);
+        velocity.scl(1 / dt);
     }
 
     public Vector2 getPosition() {
@@ -26,6 +27,10 @@ public class Boat {
 
     public Texture getTexture() {
         return texture;
+    }
+
+    public void setLane(Rectangle lane){
+        position.x = lane.getX() + lane.getWidth() / 2 - 46;
     }
 
     public void dispose(){
--- core/src/com/itayandtamir/game/States/PlayState.java
+++ core/src/com/itayandtamir/game/States/PlayState.java
@@ -1,13 +1,18 @@
 package com.itayandtamir.game.States;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.itayandtamir.game.FirstGame;
 import com.itayandtamir.game.Sprites.Boat;
-
+import com.badlogic.gdx.math.Rectangle;
 
 public class PlayState extends State {
     private static final int BOAT_Y_AXIS_START = 50;
+    private static final Rectangle LANE_LEFT = new Rectangle(0, 0, Gdx.graphics.getWidth() / 3, Gdx.graphics.getHeight());
+    private static final Rectangle LANE_MIDDLE = new Rectangle(Gdx.graphics.getWidth() / 3, 0, Gdx.graphics.getWidth() / 3, Gdx.graphics.getHeight());
+    private static final Rectangle LANE_RIGHT = new Rectangle(Gdx.graphics.getWidth() / 3 * 2, 0, Gdx.graphics.getWidth() / 3, Gdx.graphics.getHeight());
 
     private Texture background;
     private Boat boat;
@@ -20,7 +25,15 @@ public class PlayState extends State {
 
     @Override
     public void handleInput() {
-
+        if(Gdx.input.justTouched()){
+            Vector2 pressPos = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+            if(LANE_LEFT.contains(pressPos))
+                boat.setLane(LANE_LEFT);
+            else if(LANE_MIDDLE.contains(pressPos))
+                boat.setLane(LANE_MIDDLE);
+            else if(LANE_RIGHT.contains(pressPos))
+                boat.setLane(LANE_RIGHT);
+        }
     }
 
     @Override
