04650417c3215a42f38c715d74bbfb82906a9e2d
Tamir107 <tamir.razon@gmail.com>
2015-11-22 17:59:08 +0200

    Camera is now following our boat. Thanks to me. (Tamir, Itay is noob)

--- core/src/com/itayandtamir/game/States/PlayState.java
+++ core/src/com/itayandtamir/game/States/PlayState.java
@@ -1,6 +1,7 @@
 package com.itayandtamir.game.States;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -9,6 +10,7 @@ import com.itayandtamir.game.Sprites.Boat;
 import com.badlogic.gdx.math.Rectangle;
 
 public class PlayState extends State {
+
     private static final int BOAT_Y_AXIS_START = 50;
     private static final Rectangle LANE_LEFT = new Rectangle(0, 0, FirstGame.WIDTH / 3, FirstGame.HEIGHT);
     private static final Rectangle LANE_MIDDLE = new Rectangle(FirstGame.WIDTH / 3, 0, FirstGame.WIDTH / 3, FirstGame.HEIGHT);
@@ -17,12 +19,13 @@ public class PlayState extends State {
     private Texture background;
     private Boat boat;
     private Vector2 proportion;
-
     public PlayState(GameStateManager gsm) {
         super(gsm);
         proportion = new Vector2(cam.viewportWidth / Gdx.graphics.getWidth(), cam.viewportHeight / Gdx.graphics.getHeight());
         boat = new Boat(cam.viewportWidth / 2 - 46, BOAT_Y_AXIS_START);
         background = new Texture("BackgroundPlay.png");
+
+
     }
 
     @Override
@@ -42,6 +45,9 @@ public class PlayState extends State {
     public void update(float dt) {
         handleInput();
         boat.update(dt);
+        cam.position.set(cam.viewportWidth / 2, 350, 0);
+        cam.translate(0, boat.getPosition().y);
+        cam.update();
     }
 
     @Override
