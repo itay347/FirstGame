e455979c8c0dde994e8faaa18007aed048bc4d90
Tamir107 <tamir.razon@gmail.com>
2015-11-26 22:40:18 +0200

    Obstacle Class has been updated. (again)
    The obstacles are now updating every time they get out of the screen.
    They are updating by getting replaced and getting another randomed x instead of the last one.

--- core/src/com/itayandtamir/game/Sprites/Obstacle.java
+++ core/src/com/itayandtamir/game/Sprites/Obstacle.java
@@ -17,7 +17,7 @@ public class Obstacle {
     private Random rand;
 
     public Obstacle(float camY){
-        texture = new Texture("ObstacleStone");
+        texture = new Texture("ObstacleStone.png");
         pos = new Vector2(0, camY + FirstGame.HEIGHT/2 + OBSTACLE_Y_OFFSET);
         rand = new Random();
         int lane = rand.nextInt(3);
@@ -31,6 +31,19 @@ public class Obstacle {
         if (lane == 2)
             pos.x = (PlayState.LANE_RIGHT.getX() + PlayState.LANE_RIGHT.width/2 - texture.getWidth()/2);
 }
+
+    public void updatePostition(){
+        int lane = rand.nextInt(3);
+
+        if (lane == 0)
+            pos.x = (PlayState.LANE_LEFT.getX() + PlayState.LANE_LEFT.width/2 - texture.getWidth()/2);
+
+        if (lane == 1)
+            pos.x = (PlayState.LANE_MIDDLE.getX() + PlayState.LANE_MIDDLE.width/2 - texture.getWidth()/2);
+
+        if (lane == 2)
+            pos.x = (PlayState.LANE_RIGHT.getX() + PlayState.LANE_RIGHT.width/2 - texture.getWidth()/2);
+    }
 
     public void dispose(){
         texture.dispose();
--- core/src/com/itayandtamir/game/States/PlayState.java
+++ core/src/com/itayandtamir/game/States/PlayState.java
@@ -29,6 +29,10 @@ public class PlayState extends State {
         background = new Texture("BackgroundPlay.png");
         backgroundPos1 = new Vector2(0, 0);
         backgroundPos2 = new Vector2(0, background.getHeight());
+        obstacles[0] = new Obstacle(cam.position.y);
+        obstacles[1] = new Obstacle(cam.position.y);
+        obstacles[2] = new Obstacle(cam.position.y);
+
     }
 
     @Override
@@ -48,6 +52,7 @@ public class PlayState extends State {
     public void update(float dt) {
         handleInput();
         updateBackground();
+        updateObstacles();
         boat.update(dt);
         cam.position.set(cam.viewportWidth / 2, boat.getPosition().y + BOAT_Y_AXIS_OFFSET, 0);
         cam.update();
@@ -60,6 +65,9 @@ public class PlayState extends State {
         sb.draw(background, backgroundPos1.x, backgroundPos1.y, cam.viewportWidth, cam.viewportHeight);
         sb.draw(background, backgroundPos2.x, backgroundPos2.y, cam.viewportWidth, cam.viewportHeight);
         sb.draw(boat.getTexture(), boat.getPosition().x, boat.getPosition().y);
+        sb.draw(obstacles[0].getTexture(), obstacles[0].getPos().x, obstacles[0].getPos().y);
+        sb.draw(obstacles[1].getTexture(), obstacles[1].getPos().x, obstacles[1].getPos().y);
+        sb.draw(obstacles[2].getTexture(), obstacles[2].getPos().x, obstacles[2].getPos().y);
         sb.end();
     }
 
@@ -74,5 +82,21 @@ public class PlayState extends State {
             backgroundPos1.add(0, background.getHeight() * 2);
         if(cam.position.y - cam.viewportHeight / 2 > backgroundPos2.y + background.getHeight())
             backgroundPos2.add(0, background.getHeight() * 2);
+    }
+
+    private void updateObstacles(){
+        if(cam.position.y - cam.viewportHeight / 2 > obstacles[0].getPos().y + obstacles[0].getTexture().getHeight()) {
+            obstacles[0].getPos().add(0, background.getHeight() + obstacles[0].getTexture().getHeight());
+            obstacles[0].updatePostition();
+        }
+        if(cam.position.y - cam.viewportHeight / 2 > obstacles[1].getPos().y + obstacles[1].getTexture().getHeight()) {
+            obstacles[1].getPos().add(0, background.getHeight() + obstacles[1].getTexture().getHeight());
+            obstacles[1].updatePostition();
+        }
+        if(cam.position.y - cam.viewportHeight / 2 > obstacles[2].getPos().y + obstacles[2].getTexture().getHeight()) {
+            obstacles[2].getPos().add(0, background.getHeight() + obstacles[2].getTexture().getHeight());
+            obstacles[2].updatePostition();
+        }
+
     }
 }
