1a8575e8020a481d6712db420d8d679a84f4e08c
Itay Mastor <itay647@gmail.com>
2016-01-23 02:01:34 +0200

    Fixed/Changed approach of accessing stage in PlayBackgorunds class

--- core/src/com/itayandtamir/game/Actors/PlayBackgrounds.java
+++ core/src/com/itayandtamir/game/Actors/PlayBackgrounds.java
@@ -2,14 +2,11 @@ package com.itayandtamir.game.Actors;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.Align;
 import com.itayandtamir.game.Assets;
 
 public class PlayBackgrounds extends Group {
-
-    Stage stage;
 
     private Image background1, background2;
 
@@ -35,9 +32,9 @@ public class PlayBackgrounds extends Group {
     }
 
     private void updateBackgrounds() {
-        if (background1.getY(Align.top) <= stage.getCamera().position.y - stage.getCamera().viewportHeight / 2)
+        if (background1.getY(Align.top) <= getStage().getCamera().position.y - getStage().getCamera().viewportHeight / 2)
             background1.setPosition(getX(), background1.getY() + background1.getHeight() * 2);
-        if (background2.getY(Align.top) <= stage.getCamera().position.y - stage.getCamera().viewportHeight / 2)
+        if (background2.getY(Align.top) <= getStage().getCamera().position.y - getStage().getCamera().viewportHeight / 2)
             background2.setPosition(getX(), background2.getY() + background2.getHeight() * 2);
     }
 }
