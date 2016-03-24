97ead934690d04105afc86738b887c12346d655a
Tamir107 <tamir.razon@gmail.com>
2016-03-24 21:51:35 +0200

    New Skin
    New JSON File
    New Two Button on the menu screen: Start Game and Exit Game

--- core/src/com/itayandtamir/game/Screens/MenuScreen.java
+++ core/src/com/itayandtamir/game/Screens/MenuScreen.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
@@ -21,23 +23,39 @@ public class MenuScreen extends ScreenAdapter {
     private Image background;
     private ImageButton playButton;
 
+    Skin skin;
+
     public MenuScreen(FirstGame firstGame) {
         this.game = firstGame;
         stage = new Stage(new StretchViewport(FirstGame.WORLD_WIDTH, FirstGame.WORLD_HEIGHT), Assets.batch);
 
-        background = new Image(Assets.backgroundMenu);
-        playButton = new ImageButton(new TextureRegionDrawable(Assets.playButton));
-        playButton.setPosition(FirstGame.WORLD_WIDTH / 2, FirstGame.WORLD_HEIGHT / 2, Align.center);
-        playButton.addListener(new ClickListener() {
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        final TextButton button = new TextButton("Start Game", skin, "default");
+        final TextButton exitButton = new TextButton("Exit Game", skin, "default");
+        //Buttons
+        button.setWidth(200);
+        button.setHeight(50);
+        button.setPosition(FirstGame.WORLD_WIDTH / 2, FirstGame.WORLD_HEIGHT / 2, Align.center);
+        exitButton.setWidth(200);
+        exitButton.setHeight(50);
+        exitButton.setPosition(FirstGame.WORLD_WIDTH / 2, FirstGame.WORLD_HEIGHT / 2 - 80, Align.center);
+
+        button.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new PlayScreen(game));
             }
         });
-
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+        background = new Image(Assets.backgroundMenu);
         stage.addActor(background);
-        stage.addActor(playButton);
-
+        stage.addActor(button);
+        stage.addActor(exitButton);
         Gdx.input.setInputProcessor(stage);
     }
 
